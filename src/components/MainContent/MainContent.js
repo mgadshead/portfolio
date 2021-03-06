cbf164a40645880cc519cce3383d92ef7daf9bfa
Matthew Adshead <matthew.adshead+github@gmail.com>
2021-03-06 13:24:47 -0500

    horizontal scrolling

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './MainContent.scss';
 import HorizontalScroller from '../HorizontalScroller/HorizontalScroller';
 import { useScrollBoost } from 'react-scrollbooster';
+import { useHistory } from 'react-router-dom';
 
 const MainContent = props => {
     const [viewport] = useScrollBoost({
@@ -15,6 +16,22 @@ const MainContent = props => {
             }
         }
     });
+
+    const history = useHistory();
+
+    useEffect(() => {
+        let item = document.querySelector('.MainContent');
+
+        window.addEventListener('wheel', function (e) {
+            if (history.location.pathname === '/') {
+                if (e.deltaY > 0) {
+                    if (item) item.scrollLeft += 100;
+                } else if (e.deltaY < 0) {
+                    if (item) item.scrollLeft -= 100;
+                }
+            }
+        });
+    }, []);
 
     return (
         <div className='MainContent' ref={viewport}>
